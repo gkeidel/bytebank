cdbf5d274b7c3ead583d99b8156084937af3354e
Gisela Keidel <giselakeidel@gmail.com>
2021-05-27 21:36:27 -0300

    Finalizando métodos

--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -5,5 +5,22 @@ public class TestaMetodo {
 		Conta contaDoPaulo = new Conta();
 		contaDoPaulo.deposita(50);
 		System.out.println(contaDoPaulo.saldo);
+		
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		contaDoPaulo.saca(70);
+		System.out.println(contaDoPaulo.saldo);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		contaDaMarcela.transfere(300, contaDoPaulo);
+		System.out.println("saldo da Marcela: " + contaDaMarcela.saldo);
+		System.out.println("saldo do Paulo: " + contaDoPaulo.saldo);
+		
+		contaDoPaulo.titular = "Paulo Silveira";
+		System.out.println(contaDoPaulo.titular);
 	}
 }
